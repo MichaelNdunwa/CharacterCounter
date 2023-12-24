128e8eea7e044f30709aecde5c437772aa509d93
michael <ndunwa240@gmail.com>
2023-12-25 00:41:59 +0100

    count anything

---
+++ app/src/main/java/com/michael/charactercounter/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.michael.charactercounter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var editText: EditText
+    private lateinit var textView: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        editText = findViewById(R.id.editText)
+        textView = findViewById(R.id.textView)
+
+        editText.addTextChangedListener( object: TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) { }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                textView.text = "Character count: ${editText.text.length}"
+
+            }
+
+            override fun afterTextChanged(s: Editable?) { }
+
+        })
+
+    }
+}
